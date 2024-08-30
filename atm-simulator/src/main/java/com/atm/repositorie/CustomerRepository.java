49aa4eb09b453fcb10921de4cfd08e6bdd16b3aa
Dhiraj shailesh warke <157042610+Dhirajw@users.noreply.github.com>
2024-08-30 15:40:20 +0530

    Delete atm-simulator directory

--- atm-simulator/src/main/java/com/atm/repositorie/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atm.repositorie;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.atm.entities.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-
-}
